5f7c2ac48ab04b683e3ab3ae99b5a37b64f68fda
Jared <jaredflomen@gmail.com>
2020-10-26 20:06:07 +0000

    Testing Assertation

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("💚💚💚 Assertation Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("🚨🚨🚨 Assertation Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 211);
+assertEqual("Jared", "Jared");
