5b05f8f9c3af810e425bf23a7f37ed42eb81fc37
Jared <jaredflomen@gmail.com>
2020-10-27 20:39:58 +0000

    Taking two arrays and logging a message to the console

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(firstArray, secondArray) {
+  let test = true;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      test = false;
+      return test;
+    }
+  }
+  return test;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertation Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🚨🚨🚨 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
