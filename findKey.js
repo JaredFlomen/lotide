76e0afb83b3227971208363909bd3522b74a0ba5
Jared <jaredflomen@gmail.com>
2020-10-29 22:23:15 +0000

    Scanning an object and returning the key for which the callback is true

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const findKey = function (search, callback){
+  for (const i in search){
+    if (callback(search[i])){ 
+      return i; 
+    }
+  }
+}
+
+const data = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertation Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🚨🚨🚨 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(data, "noma");
+assertEqual(data, "jared");
+
