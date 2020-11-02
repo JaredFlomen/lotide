e7bcaec12eea109bd35f931a17614a48d0f0e9be
Jared <jaredflomen@gmail.com>
2020-11-02 17:49:34 +0000

    Updating exporting assertion tests

--- head.js
+++ head.js
@@ -6,17 +6,17 @@
 //   }
 // };
 
-const assertEqual = require('./assertEqual');
+// const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
+
+module.exports = head;
 
 //assertEqual("Lighthouse Labs", "Bootcamp");
 // //assertEqual(1, 1);
 // assertEqual(1, 211);
 // assertEqual("Jared", "Jared");
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
