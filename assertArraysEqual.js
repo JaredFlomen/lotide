7b58fef26b8f6d660bf8c151cd62f16b437a29ff
Jared <jaredflomen@gmail.com>
2020-11-02 18:21:53 +0000

    Refactoring with exports and require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,6 @@
-const eqArrays = function(firstArray, secondArray) {
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
+
+
 
 const assertArraysEqual = function(actual, expected) {
   if (actual === expected) {
