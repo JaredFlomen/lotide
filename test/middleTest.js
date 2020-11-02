3ae87c6c9984f4dcdd54c6b6787655f64cd04b33
Jared <jaredflomen@gmail.com>
2020-11-02 19:26:46 +0000

    Updating middleTest with chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,18 @@
-const middle = require('../middle')
-const eqArrays = require('../eqArrays')
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
+const middle = require('../middle');
 
-assertArraysEqual(eqArrays(middle([1]), [1]), true);
-assertArraysEqual(eqArrays(middle([1, 2]), [1, 2]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
+describe('#middleTest', () => {
+  it('Should return [2] when passed [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('Should return [2, 3] when passed [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it('Should return [3, 4] when passed [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
+
