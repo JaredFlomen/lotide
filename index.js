1618abe52ab334e26e4f5c2eb09b1d20e1fcaed5
Jared <jaredflomen@gmail.com>
2020-11-02 20:57:19 +0000

    An index of all utility functions

--- index.js
+++ index.js
@@ -2,10 +2,37 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 const without = require('./without');
+const map = require('./map');
+const findKey = require('./findKey');
+const takeUntil = require('./takeUntil');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqObjects = require('./eqObjects');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 module.exports = {
   head: head,
   tail: tail,
   middle: middle,
-  without: without
-};
+  without: without,
+  map: map,
+  findKey: findKey,
+  takeUntil: takeUntil,
+  letterPositions: letterPositions,
+  findKeyByValue: findKeyByValue,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  eqObjects: eqObjects,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  assertArraysEqual: assertArraysEqual
+}
