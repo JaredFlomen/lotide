ad8a207ee3ca11d90507027b556c95077e74c690
Jared <jaredflomen@gmail.com>
2020-11-02 21:07:33 +0000

    Shortening index.js

--- index.js
+++ index.js
@@ -12,27 +12,25 @@ const countOnly = require('./countOnly');
 const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 const eqObjects = require('./eqObjects');
-const assertEqual = require('./assertEqual');
-const eqArrays = require('./eqArrays');
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  without: without,
-  map: map,
-  findKey: findKey,
-  takeUntil: takeUntil,
-  letterPositions: letterPositions,
-  findKeyByValue: findKeyByValue,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  eqObjects: eqObjects,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual
+  head,
+  tail,
+  middle,
+  without,
+  map,
+  findKey,
+  takeUntil,
+  letterPositions,
+  findKeyByValue,
+  countLetters,
+  countOnly,
+  assertEqual,
+  assertObjectsEqual,
+  eqObjects,
+  eqArrays,
+  assertArraysEqual
 }
+
